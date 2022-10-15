6c1910c41ee2fb030f1b942e6e90e949ef9fb94f
KrisAirdancer <chris.scott.marston@gmail.com>
2022-10-15 16:20:19 -0600

    The track scrubber is almost complete.

--- vidya-frontend/src/Navbar.js
+++ vidya-frontend/src/Navbar.js
@@ -4,26 +4,34 @@ import React from 'react'
 
 export default function HelloWorld() {
   return (
-    <nav className="nav-flex-container">
+    <div>
+      <nav className="nav-flex-container">
 
-      <select id="nav-playlist-selector">
-        <option value="playlist_vip">VIP</option>
-        <option value="playlist_source" selected>Source</option>
-      </select>
+        <select id="nav-playlist-selector">
+          <option value="playlist_vip">VIP</option>
+          <option value="playlist_source" selected>Source</option>
+        </select>
 
-      <div id="track-info head">Track Info</div>
+        <div id="track-info head">Track Info</div>
 
-      <ul>
-        <li>
-          <button id="btn_chosen" className="nav-button">C</button>
-        </li>
-        <li>
-          <button id="btn_exiled" className="nav-button">E</button>
-        </li>
-        <li>
-          <button id="btn_volume" className="nav-button">V</button>
-        </li>
-      </ul>
-    </nav> 
+        <ul>
+          <li>
+            <button id="btn_chosen" className="nav-button">C</button>
+          </li>
+          <li>
+            <button id="btn_exiled" className="nav-button">E</button>
+          </li>
+          <li>
+            <button id="btn_volume" className="nav-button">V</button>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="track-scrubber-flex-container">
+        <label id="time-played">0:00</label>
+        <input type="range" id="track-scrubber"/>
+        <label id="time-left">3:42</label>
+      </div>
+    </div>
   )
 }
