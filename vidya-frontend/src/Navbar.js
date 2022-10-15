2c2a3fc7051267fb7b93e1903662a41c69d271cb
KrisAirdancer <chris.scott.marston@gmail.com>
2022-10-15 14:38:25 -0600

    The navbar content is now arranged properly.

--- vidya-frontend/src/Navbar.js
+++ vidya-frontend/src/Navbar.js
@@ -1,18 +1,27 @@
 import React from 'react'
 
+// TODO: For the <select> tag below, React.js requires that the values of the select dropdown be set using props. I will need to change my HTML to match the proper React.js formatting instead of vanilla HTML formatting. See this: https://stackoverflow.com/questions/52143691/react-warning-use-the-defaultvalue-or-value-props-on-select-instead-ofhttps://stackoverflow.com/questions/52143691/react-warning-use-the-defaultvalue-or-value-props-on-select-instead-of 
+
 export default function HelloWorld() {
   return (
-    <nav class="navbar">
-      <div>Track Info</div>
+    <nav className="nav-flex-container">
+
+      <select id="nav-playlist-selector">
+        <option value="playlist_vip">VIP</option>
+        <option value="playlist_source" selected>Source</option>
+      </select>
+
+      <div id="track-info head">Track Info</div>
+
       <ul>
         <li>
-          <button id="btn_chosen" class="nav-button">C</button>
+          <button id="btn_chosen" className="nav-button">C</button>
         </li>
         <li>
-          <button id="btn_exiled" class="nav-button">E</button>
+          <button id="btn_exiled" className="nav-button">E</button>
         </li>
         <li>
-          <button id="btn_volume" class="nav-button">V</button>
+          <button id="btn_volume" className="nav-button">V</button>
         </li>
       </ul>
     </nav> 
