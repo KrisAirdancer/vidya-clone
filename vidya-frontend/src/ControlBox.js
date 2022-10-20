a55a09e808a760f1449d6e9a20f3fa38cedd5222
KrisAirdancer <chris.scott.marston@gmail.com>
2022-10-20 09:52:54 -0600

    Added buttons and basic styling to control box. Need to finish styling.

--- vidya-frontend/src/ControlBox.js
+++ vidya-frontend/src/ControlBox.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function ControlBox() {
   return (
-    <div id='control-box-flex-container'>
+    <div id='control-box-flex-container' className='control-box'>
       
+      <div className='control-box-row control-box-element'>Drag me!</div>
+
+      <div id='control-box-row-two-flex-container' className='control-box-second-row'>
+        <button className='control-box-element'>&lt;&lt;</button>
+        <button className='control-box-element'>&gt;/II</button>
+        <button className='control-box-element'>&gt;&gt;</button>
+      </div>
+
+      <div id='control-box-row-three-flex-container' className='control-box-row'>
+        <button className='control-box-element'>Replay</button> {/* TODO: This button should set the current track to replay endlessly */}
+        <button className='control-box-element'>Nav Up</button> {/* TODO: This button should hide the navbar */}
+      </div>
+    
     </div>
   )
 }
