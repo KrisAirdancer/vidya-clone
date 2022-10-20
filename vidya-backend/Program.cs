e03af174198e59b33e190ef1a470cb14262dd329
KrisAirdancer <chris.scott.marston@gmail.com>
2022-10-20 15:09:41 -0600

    Attempting to use UseSpa from .NET. No success yet.

--- vidya-backend/Program.cs
+++ vidya-backend/Program.cs
@@ -1,5 +1,21 @@
+// using Microsoft.AspNetCore.SpaServices.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// app.UseStaticFiles();
+
+    app.UseStaticFiles();
+    app.UseSpaStaticFiles();
+
+   
+    app.UseSpa(spa =>
+    {
+        if (env.IsDevelopment())
+        {
+            spa.UseProxyToSpaDevelopmentServer("http://localhost:4000");
+        }
+    });
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/endpoint-test", () => "The endpoint connection works!");
