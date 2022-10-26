4f24bd2187e59d2d8fa87065b7551ee658ca2055
KrisAirdancer <chris.scott.marston@gmail.com>
2022-10-25 20:07:53 -0600

    Converted project to a single file and added JQuerry.

--- vidya-backend/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/endpoint-test", () => "The endpoint connection works!");
-
-app.Run();
---
+++ vidya-frontend/public/scripts.js
@@ -0,0 +1,3 @@
+$(docuemnt).ready(() => {
+    
+})
