016eec8c6c79383cd371bba8299492aab9fe3b52
KrisAirdancer <chris.scott.marston@gmail.com>
2022-10-25 20:37:01 -0600

    Removed JQuery and separated CSS, HTML, and JavaScript into separate files.

--- vidya-frontend/public/scripts.js
+++ vidya-frontend/public/scripts.js
@@ -1,3 +1,11 @@
-$(docuemnt).ready(() => {
+let btn1 = () => {
+    console.log('here')
+    let e = document.querySelector('#btn_chosen')
     
-})
+    if (e.style.color === 'red') {
+        e.style.color = '#FFA366';
+        console.log(e.style.color)
+    } else {
+        e.style.color = 'red';
+    }
+}
