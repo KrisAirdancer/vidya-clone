fbd69bed0221ac1886fa9b61471975c8abfa4cd2
KrisAirdancer <chris.scott.marston@gmail.com>
2022-11-09 17:59:06 -0700

    Setting up files and directories.

--- vidya-backend/app.js
+++ vidya-backend/app.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const app = express()
+
+const PORT = 11002;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+
+
+
+
+
 // TODO: Build Node.js server here.
--- vidya-frontend/app.js
+++ vidya-frontend/app.js
@@ -1,3 +1,20 @@
+const express = require('express')
+const app = express()
+
+const PORT = 11001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+
+
+
+
 // TODO: Build Node.js server here.
 // TODO: Restructure public to have public/tracks and public/resources
     // public/resources should contain .css files, images, favicons, etc.
