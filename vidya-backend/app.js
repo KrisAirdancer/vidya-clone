6badb51fa787fdd013b4076dbd2194b4654beb73
KrisAirdancer <chris.scott.marston@gmail.com>
2022-11-12 14:35:10 -0700

    Implemented /list/:listName route.

--- vidya-backend/app.js
+++ vidya-backend/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 /***** CONFIGURATIONS *****/
 
 const PORT = 11001;
-
-/***** ROUTING *****/
+const TRACK_LISTS_DIR = './data';
 
 /***** APPLICATION SETUP *****/
 
@@ -24,13 +24,46 @@ app.use(express.static(path.join(__dirname, 'public')));
  */
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.get('/list/:listName', (req, res) => {
+
+  let listName = '';
+  let status = 400;
+
+  switch(req.params.listName) {
+    case 'master':
+      listName = 'tracks-master-list.json'
+      status = 200;
+      break;
+    case 'chosen':
+      listName = 'chosen-tracks.json'
+      status = 200;
+      break;
+    case 'exiled':
+      listName = 'exiled-tracks.json'
+      status = 200;
+      break;
+    default:
+      status = 404;
+      break;
+  }
+
+  if (status !== 400 && status !== 404) {
+    
+    let readStream = fs.createReadStream(TRACK_LISTS_DIR + '/' + listName);
+    
+    res.status(200);
+    readStream.pipe(res);
+
+  } else {
+    res.status(status);
+    res.send();
+  }
+});
 
 /***** LAUNCH APPLICATION *****/
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`vidya-backend is listening on port ${PORT}`)
 })
-
-
-// TODO: Build Node.js server here.
