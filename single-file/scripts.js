2d4fc4bce383db608489a73cd5de8d42739e6363
KrisAirdancer <chris.scott.marston@gmail.com>
2023-01-03 10:43:57 -0700

    Added styles.css and scripts.js.

---
+++ single-file/scripts.js
@@ -0,0 +1,10 @@
+/// TODO: This is a test function. Remove it.
+let btn1 = () => {
+    let e = document.querySelector('#btn_chosen')
+    
+    if (e.style.color === 'red') {
+        e.style.color = '#FFA366';
+    } else {
+        e.style.color = 'red';
+    }
+}
