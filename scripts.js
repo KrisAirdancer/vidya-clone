1fd579d2927d84667932b4ff69c1bc5f7d15e262
KrisAirdancer <chris.scott.marston@gmail.com>
2023-06-17 10:22:02 -0600

    Cleaned up comments.

--- scripts.js
+++ scripts.js
@@ -745,8 +745,8 @@ function playPreviousTrack()
 
   updateNavButtons();
 
-  console.log(currentTrack.trackID);
-  console.log(currentTrack);
+  // console.log(currentTrack.trackID);
+  // console.log(currentTrack);
 }
 
 // TODO: This function appears to be unused! Either use it somewhere or delete it. It looks like this function was replaced by getRandomTrackID() - instead of calling a function to play the next track, I am calling a function to generate a track ID. I think I did this because I needed to be able to play tracks under different conditions in different places. Double check this and refactor if it doesn't make sense to do that anymore (or if that isn't the case).
